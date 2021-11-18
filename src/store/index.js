deeca2691278f61a8cbb77e8814ba6b9fc6183c2
Rohan Kurian <rohankurian10@hotmail.com>
2021-11-18 23:02:03 +0530

    added store

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,23 @@
 import { createStore } from "redux";
 
-let initState = {};
+let initState = {
+  counter: 0,
+  flag: true,
+};
+
+const counterReducer = (state = { initState }, action) => {
+  if (action.type === "INC") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DEC") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
