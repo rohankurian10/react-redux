459aafc8f96edecfc599f1be76958067296e8ff4
Rohan Kurian <rohankurian10@hotmail.com>
2021-11-18 22:32:04 +0530

    Did styling to centeredcontainer

--- src/components/CenterContainer.js
+++ src/components/CenterContainer.js
@@ -1,7 +1,21 @@
 import "./CenteredContainer.css";
 
 const CenterContainer = () => {
-  return <div className="CenteredContainer"></div>;
+  return (
+    <div className="CenteredContainer">
+      <div className="TitleContainer">
+        <h2>REDUX COUNTER</h2>
+      </div>
+      <div className="CounterContainer">
+        <p>0</p>
+      </div>
+      <div className="ButtonContainer">
+        <button>Increment</button>
+        <button>Change</button>
+        <button>Decrement</button>
+      </div>
+    </div>
+  );
 };
 
 export default CenterContainer;
